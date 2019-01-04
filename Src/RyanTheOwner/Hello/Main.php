e62bd1cbc462f9aa960ed52d81e44fdd5f14c375
RyanTheOwner <39419511+RyanTheOwner@users.noreply.github.com>
2019-01-04 11:59:32 -0800

    Update Main.php

--- Src/RyanTheOwner/Hello/Main.php
+++ Src/RyanTheOwner/Hello/Main.php
@@ -1,12 +1,22 @@
 <?php
 namespace RyanTheOwner\Hello;
+
 use pocketmine\Player;
-use pocketmine\event\PlayerJoinEvent;
-class Main{
-   public function _construct(Player $player, PlayerJoinEvent $event) {
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\Listener;
+/** A plugin made by RyanTheOwner w/ the help of Raccoon Undercover. I am a developer in progress and hope to proceed greatly.
+*/
+class Main implements Listener{
+
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+ 
+   public function _construct(Player $player, PlayerJoinEvent $event){
      $this->player = $player;
      $this->playerJoinEvent = $event;
    }
+   
    public function onPlayerJoinEvent(){
      $player->$event;
      $player->sendMessage("Hello!");
