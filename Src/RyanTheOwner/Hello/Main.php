d5fcfe5b66b301dc964edaf27de8ffae48ba92e5
RyanTheOwner <39419511+RyanTheOwner@users.noreply.github.com>
2019-01-03 22:46:44 -0800

    Create Main.php

---
+++ Src/RyanTheOwner/Hello/Main.php
@@ -0,0 +1,14 @@
+<?php
+namespace RyanTheOwner\Hello;
+use pocketmine\Player;
+use pocketmine\event\PlayerJoinEvent;
+class Main{
+   public function _construct(Player $player, PlayerJoinEvent $event) {
+     $this->player = $player;
+     $this->playerJoinEvent = $event;
+   }
+   public function onPlayerJoinEvent(){
+     $player->$event;
+     $player->sendMessage("Hello!");
+   }
+}
